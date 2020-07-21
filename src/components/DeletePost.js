13cffda31b23594cfdf4fc9aa8a05c220a73500d
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-07-21 15:04:17 -0700

    Add blog

---
+++ src/components/DeletePost.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DeletePost extends Component {
+    render() {
+        return (
+            <button>Delete</button>
+        )
+    }
+}
+
+export default DeletePost;
---
+++ src/components/DisplayPosts.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { listPosts } from '../graphql/queries';
+import { API, graphqlOperation } from 'aws-amplify';
+import DeletePost from './DeletePost';
+import EditPost from './EditPost';
+
+class DisplayPosts extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount = async () => {
+        this.getPosts()
+    }
+
+    getPosts = async () => {
+        const result = await API.graphql(graphqlOperation(listPosts))
+
+        this.setState({ posts: result.data.listPosts.items})
+        //console.log("All Posts: ", JSON.stringify(result.data.listPosts.items))
+    }
+
+    render() {
+        const { posts } = this.state
+        return posts.map(( post ) => {
+
+            return (
+                <div className="posts" style={rowStyle} key={ post.id }>
+                    <h1> { post.postTitle } </h1>
+                    <span style={{ fontStyle: "italic", color: "#0ca5e297" }}>
+                        { "Written by: " } { post.postOwnerUsername }
+
+                        {" on "}
+                        <time style={{ fontStyle: "italic" }}>
+                            {" "}
+                            { new Date(post.createdAt).toDateString() }
+                        </time>
+                    </span>
+                    <p> { post.postBody } </p>
+                    <br />
+                    <span>
+                        <DeletePost />
+                        <EditPost />
+                    </span>
+                </div>
+            )
+
+        })
+    }
+}
+
+const rowStyle = {
+    background: '#f4f4f4',
+    padding: '10px',
+    border: '1px #ccc dotted',
+    margin: '14px'
+}
+
+export default DisplayPosts;
